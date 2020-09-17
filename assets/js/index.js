22788be641a3b95583c94da0ea64f63ed5c3946b
Sabiha <fosabiha@gmail.com>
2020-09-17 10:23:27 +0000

    magic.html is coaded

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,24 @@ $(document).ready(function(){
         alert("It's just a game for the people who beleives in luck!");
     });
     });
+
+
+/*--morphing pop up menu--*/
+let menu = document.querySelector(".menu");
+let button = document.querySelector(".menu__button");
+
+toggleMenu = () => {
+  menu.classList.toggle("open");
+}
+
+button.addEventListener("click", function() {
+  clearInterval(interactionPreview);
+  toggleMenu();
+});
+
+let interactionPreview = setInterval(() => {
+  toggleMenu();
+}, 2000)
+
+
+/*--quiz--*/
